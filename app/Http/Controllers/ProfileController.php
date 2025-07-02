512842da0839e87998a833503e8ffc177d715a76
geavonny <geavonny8@yahoo.com>
2025-07-02 18:47:05 +0700

    Endpoint 16th Version
    Function Update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -200,34 +200,55 @@ class ProfileController extends Controller
     public function destroy($no_rekam_medis)
     {
             $profile = Profile::where('no_rekam_medis',$no_rekam_medis)->first();
-                $profile->delete();
-            
+                if($profile){
+                    $profile->delete();
+                    return response()->json([
+                    'success' => true,
+                    'message' => 'Profile pasien berhasil dihapus!',
+                ], 200);
+                }    
             $histori = Histori::where('no_rekam_medis',$no_rekam_medis)->first();
                 if($histori){
                     $histori->delete();
+                    return response()->json([
+                    'success' => true,
+                    'message' => 'Histori pasien berhasil dihapus!',
+                ], 200);
                 }                
             $diagnosis = Diagnosis::where('no_rekam_medis',$no_rekam_medis)->first();
                 if($diagnosis){
-                    $diagnosis->delete();        
+                    $diagnosis->delete();
+                    return response()->json([
+                    'success' => true,
+                    'message' => 'Diagnosis pasien berhasil dihapus!',
+                ], 200);       
                 }                 
             
             $rujukan = Rujukan::where('no_rekam_medis',$no_rekam_medis)->first();
                 if($$rujukan){
-                    $rujukan->delete();        
+                    $rujukan->delete();
+                    return response()->json([
+                    'success' => true,
+                    'message' => 'Rujukan pasien berhasil dihapus!',
+                ], 200);        
                 }             
                 
             $rekam = Rekam::where('no_rekam_medis',$no_rekam_medis)->first();
                 if($$rekam){
-                    $rekam->delete(); 
+                    $rekam->delete();
+                    return response()->json([
+                    'success' => true,
+                    'message' => 'Rekam Medis pasien berhasil dihapus!',
+                ], 200);
                 }         
                
 
-            if ($profile && $histori && $rujukan && $rekam && $diagnosis) {
-            // if ($profile){
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Profile pasien berhasil dihapus!',
-                ], 200);
-            }                    
+            // if ($profile && $histori && $rujukan && $rekam && $diagnosis) {
+            // // if ($profile){
+            //     return response()->json([
+            //         'success' => true,
+            //         'message' => 'Profile pasien berhasil dihapus!',
+            //     ], 200);
+            // }                    
     }
 }
